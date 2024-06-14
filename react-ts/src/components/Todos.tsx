17acbf0c3380e02413176883c3e8d1870dbcc1e6
Sergio <norgestream@gmail.com>
2024-06-14 11:28:21 +0300

    create first generic type in React.FC

---
+++ react-ts/src/components/Todos.tsx
@@ -0,0 +1,11 @@
+const Todos: React.FC<{ todos: string[] }> = (props) => {
+  return (
+    <ul>
+      {props.todos.map(todo => (
+        <li key={todo}>{todo}</li> 
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
